1ea4d0166629503b985c59364f41717c866bbb53
Anton Volkov <choixer@gmail.com>
2012-08-30 02:12:52 +0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yttest::Application.config.secret_token = '8100d08d7871885eca0261f8ce8fde87fdf3c2dab23eee4044f4dff950a9220743989395e215f65169a184e01d4fdb53f83500f341fcbe262f8cfcc7200e3e95'
